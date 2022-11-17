39d35fc9dce8f2a0df9f3165319869d306647c99
Tony Hirst <tony.hirst@gmail.com>
2022-11-17 20:08:16 +0000

    Update xml_utils.py

--- src/xml_utils.py
+++ src/xml_utils.py
@@ -6,6 +6,9 @@ import re
 
 def context_path(xpath):
     """Simplify an xpath to give a cleaner context path."""
+    # The xpath can be retrieved from an element by:
+    # tree = etree.ElementTree(root)
+    # tree.getpath(el)
     xpath = re.sub(r'\[\d+\]', '', xpath)
     xpath = re.sub(r'(/Item|/Unit|/Session|/Section|/SubSection|/InternalSection)', '', xpath)
     return xpath
@@ -13,11 +16,22 @@ def context_path(xpath):
 def create_id(records, id_field="id", fields=["code", "name"]):
     """Create a key from the specified fields."""
     fields = [fields] if isinstance(fields, str) else fields
-    for record in records:
+    if isinstance(records, list):
+        for record in records:
+            key = "-".join([record[k] for k in fields ])
+            # We could check there is no collision on the id key
+            record[id_field] = hashlib.sha1(key.encode(encoding='UTF-8')).hexdigest()
+    elif isinstance(records, dict):
         key = "-".join([record[k] for k in fields ])
         # We could check there is no collision on the id key
-        record[id_field] = hashlib.sha1(key.encode(encoding='UTF-8')).hexdigest()    
-    # The dict is updated directly so we have no return value
+        record[id_field] = hashlib.sha1(key.encode(encoding='UTF-8')).hexdigest()
+    elif isinstance(records, tuple):
+        key = "-".join([k for k in records])
+        # We could check there is no collision on the id key
+        return hashlib.sha1(key.encode(encoding='UTF-8')).hexdigest()
+      
+    # The records list or dict is mutable and is updated directly
+    # Consequently, we do not need a return value
 
 def unpack(x):
     return etree.tostring(x)
